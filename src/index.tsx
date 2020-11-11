7f05a92a49582d99d5dfdff858a354919393df56
PeresMiguel <msouza@realtec.com.br>
2020-11-11 08:30:32 -0300

    Log Correção

--- src/index.tsx
+++ src/index.tsx
@@ -3,4 +3,8 @@ import './defaultStyle.css';
 import ReactDOM from 'react-dom';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css';
+console.log("Bem vindo ao Martins!")
+console.log = () =>{}
+console.error = () =>{}
+console.warn = () =>{}
 ReactDOM.render(<App />, document.getElementById('root'));

50964c3066f0903e7bde676551cc6fde5abcab54
PeresMiguel <msouza@realtec.com.br>
2020-11-11 10:53:24 -0300

    setInterval

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,34 @@
-import React from 'react';
-import './defaultStyle.css';
-import ReactDOM from 'react-dom';
-import App from './App';
-import 'bootstrap/dist/css/bootstrap.min.css';
-console.log("Bem vindo ao Martins!")
-console.log = () =>{}
-console.error = () =>{}
-console.warn = () =>{}
-ReactDOM.render(<App />, document.getElementById('root'));
+import React from "react";
+import "./defaultStyle.css";
+import ReactDOM from "react-dom";
+import App from "./App";
+import "bootstrap/dist/css/bootstrap.min.css";
+console.log("Bem vindo ao Martins!");
+console.log = () => {};
+console.error = () => {};
+console.warn = () => {};
+window.addEventListener(
+  "message",
+  function receiveMessage(event: any) {
+    var window_data = {
+      window_height: document.body.scrollHeight,
+    };
+
+    event.source.postMessage(window_data, event.origin);
+  },
+  false
+);
+
+setInterval(function() {
+  var wrapperHeight:any =  document.getElementsByClassName("website-wrapper")[0]
+  if(wrapperHeight)
+    wrapperHeight = wrapperHeight.offsetHeight + 30;
+
+  var data = {
+    height: wrapperHeight,
+    origin: "https://parceiro.martins.com.br",
+  };
+
+  window.parent.postMessage(data, "*");
+}, 500);
+ReactDOM.render(<App />, document.getElementById("root"));
